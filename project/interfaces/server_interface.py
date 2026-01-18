ce368c6c2f0315477f28632728c31393c353eda1
Leon Kleinschmidt <leon.kleinschmidt@outlook.de>
2026-01-18 15:16:45 +0100

    Server: add message fetching & posting

--- project/interfaces/server_interface.py
+++ project/interfaces/server_interface.py
@@ -9,9 +9,21 @@ class KeyBundle:
     onetime_prekey: str
 
 
+@dataclass
+class Message:
+    identity_key: str
+    ephemeral_key: str
+
+
 class ServerInterface(ABC):
     @abstractmethod
     def publish_prekeys(self, name: str, bundle: KeyBundle): ...
 
     @abstractmethod
     def fetch_prekeys(self, name: str) -> KeyBundle | None: ...
+
+    @abstractmethod
+    def fetch_message(self, name: str) -> Message | None: ...
+
+    @abstractmethod
+    def post_message(self, recipient: str, message: Message): ...
--- project/server.py
+++ project/server.py
@@ -1,12 +1,19 @@
-from interfaces.server_interface import KeyBundle, ServerInterface
+from interfaces.server_interface import KeyBundle, Message, ServerInterface
 
 
 class Server(ServerInterface):
     def __init__(self):
         self.prekey_bundles = {}
+        self.messages = {}
 
     def publish_prekeys(self, name: str, bundle: KeyBundle):
         self.prekey_bundles[name] = bundle
 
     def fetch_prekeys(self, name: str) -> KeyBundle | None:
         return self.prekey_bundles.get(name)
+
+    def fetch_message(self, name: str) -> Message | None:
+        return self.messages.get(name)
+
+    def post_message(self, recipient: str, message: Message):
+        self.messages[recipient] = message
